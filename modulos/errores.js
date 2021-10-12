e2c3dd16735c7cc40bb518cff98ff6a32fdb7266
Alexander_Acosta <alexanderacosta@unach.cl>
2021-10-12 13:00:35 -0300

    Avance en el manejo de errores

---
+++ modulos/errores.js
@@ -0,0 +1,31 @@
+function otraFuncion() {
+    serompe();
+}
+
+function serompe() {
+    return 3 + z;
+}
+
+function seRompeAsincrona(cb) {
+    setTimeout(function () {
+        try {
+            return 3 + z;
+        } catch (err) {
+            console.error('Error en mi función asíncrona')
+            cb(err);
+        }
+    })
+}
+
+try {
+    //otraFuncion();
+    seRompeAsincrona(function (err) {
+        console.log (err.message)
+    });
+} catch(err) {
+    console.error('Vaya, algo se ha roto...')
+    console.error(err.message);
+    console.log('Pero no pasa nada, lo hemos capturado')
+}
+
+console.log('Esto de aquí está al final');
