4cde3a63dfa71c468d7e6fe66e5bfe2696b879fc
Alexander_Acosta <alexanderacosta@unach.cl>
2021-10-12 16:13:47 -0300

    Avances de clase

---
+++ modulos/procesoHijo.js
@@ -0,0 +1,27 @@
+const { exec, spawn } = require('child_process');
+
+// exec('node modulos/consola.js', (err, stdout, stderr) => {
+//     if (err) {
+//         console.error(err);
+//         return false;
+//     }
+// 
+//     console.log(stdout);
+// 
+// });
+
+let proceso = spawn ('ls', ['-la']);
+
+console.log(proceso.pid);
+console.log(proceso.connected);
+
+proceso.stdout.on('data', function (dato) {
+    console.log('¿Está muerto?');
+    console.log(process.killed);
+    console.log(dato.toString());
+});
+
+proceso.on('exit', function() {
+    console.log('El proceso ha terminado');
+    console.log(proceso.killed)
+})
